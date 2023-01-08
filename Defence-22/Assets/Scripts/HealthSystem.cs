48b0287af1bc70b0cd742b981a599f3a324f40e2
Joey Wang <joeywangzr@gmail.com>
2023-01-08 18:55:28 -0500

    (#1) Added health system

---
+++ Defence-22/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI healthText;
+
+    private int _healthAmount = 100;
+
+    public static HealthSystem Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateHealth(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateHealth(int change)
+    {
+        _healthAmount += change;
+        healthText.text = _healthAmount.ToString();
+    }
+}
